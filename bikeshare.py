0ba233594d8ab6796f89decd5b296765a3b8c30a
Deeptansh Aggarwal <deeptansh.sss@gmail.com>
2023-08-14 05:18:08 +0530

    Input message update for main function

--- bikeshare.py
+++ bikeshare.py
@@ -172,7 +172,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         display_data(df)
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Enter yes or no to proceed.\n')
         if restart.lower() != 'yes':
             break
 
